311466aadbcb95351e29fd18b13ed1f902b3d75a
RubyGonzalez26 <rubylol26@gmail.com>
2025-11-03 23:14:57 -0600

    feature: Set up PagesController and root route for homepage

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+# new controller class PagesController
+# inherited all the basic functionallity from ApplicationController
+# def index - action/method that root route (root 'pages#index') directs to
+class PagesController < ApplicationController  
+  def index
+  end
+end
